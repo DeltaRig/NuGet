5ec0488a637bb64562ef1629178fe98162f4e0ab
AMERICAS\Daniela_Rigoli <daniela_rigoli@dell.com>
2021-10-27 13:55:28 -0300

    Console App

---
+++ NuGet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace NuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JArray array = new JArray();
+            JValue text = new JValue("Manual Text");
+            JValue date = new JValue(new DateTime(2000, 5, 23));
+
+            array.Add(text);
+            array.Add(date);
+
+            string json = array.ToString();
+            Console.WriteLine(json);
+
+            Console.ReadLine(); // just to dont close the program
+        }
+    }
+}
