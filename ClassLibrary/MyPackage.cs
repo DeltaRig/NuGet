d7c4d36dbaab8726849f855e13de8c1144c89d24
AMERICAS\Daniela_Rigoli <daniela_rigoli@dell.com>
2021-10-28 16:06:03 -0300

    Add library

---
+++ ClassLibrary/MyPackage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class MyPackage
+    {
+        public void DisplayTest(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NuGet
 NuGet is a Visual Studio extension that makes it easy for developers to create, share, and consume useful code.
+
+## Environment:
+
+https://www.nuget.org/packages/Newtonsoft.Json/ 
+
+> Install-Package Newtonsoft.Json -Version 13.0.1 
+
